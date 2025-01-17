0f2f08f8a818e171e8a2d534418f801d7bde3dd9
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2025-01-17 18:04:26 +0200

    Update post-renew.sh

--- web/certbot/renewal-hooks/post-renew.sh
+++ web/certbot/renewal-hooks/post-renew.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Перезавантаження Nginx після оновлення сертифіката
 docker exec -it nginx-container-name nginx -s reload
 
 echo "Nginx has been reloaded to apply the updated certificate."
